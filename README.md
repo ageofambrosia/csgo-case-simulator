cf7374ad3c747d9aabb2b8793ccf4afdfaf3725c
Vidhan <vidh.bhatt@gmail.com>
2019-11-28 15:05:11 -0500

    add esports 2013 case

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # CS:GO Case Simulator
+
 Feel like you're losing money without actually losing it!
+
+## Cases
+
+- [CS:GO Weapon Case](https://csgostash.com/case/1/CS:GO-Weapon-Case) [Q]
+- [eSports 2013 Case](https://csgostash.com/case/2/eSports-2013-Case) [W]
---
+++ modules/cases/esports_2013_case.py
@@ -0,0 +1,204 @@
+case_full_name = "eSports 2013 Case"
+case_name = "esports_2013_case"
+
+skin1 = {
+    "weapon": "FAMAS",
+    "skin": "Doomkitty",
+    "rarity": "Mil-Spec",
+    "float_range": [0.08, 0.22],
+    "type": "gun",
+    "id": 1,
+}
+
+skin2 = {
+    "weapon": "MAG-7",
+    "skin": "Memento",
+    "rarity": "Mil-Spec",
+    "float_range": [0.02, 0.18],
+    "type": "gun",
+    "id": 2,
+}
+
+skin3 = {
+    "weapon": "M4A4",
+    "skin": "Faded Zebra",
+    "rarity": "Mil-Spec",
+    "float_range": [0.06, 0.80],
+    "type": "gun",
+    "id": 3,
+}
+
+skin4 = {
+    "weapon": "Sawed-Off",
+    "skin": "Orange DDPAT",
+    "rarity": "Restricted",
+    "float_range": [0.06, 0.80],
+    "type": "gun",
+    "id": 4,
+}
+
+skin5 = {
+    "weapon": "P250",
+    "skin": "Splash",
+    "rarity": "Restricted",
+    "float_range": [0.06, 0.18],
+    "type": "gun",
+    "id": 5,
+}
+
+skin6 = {
+    "weapon": "Galil AR",
+    "skin": "Orange DDPAT",
+    "rarity": "Restricted",
+    "float_range": [0.06, 0.80],
+    "type": "gun",
+    "id": 6,
+}
+
+skin7 = {
+    "weapon": "AWP",
+    "skin": "BOOM",
+    "rarity": "Classified",
+    "float_range": [0.06, 0.28],
+    "type": "gun",
+    "id": 7,
+}
+
+skin8 = {
+    "weapon": "AK-47",
+    "skin": "Red Laminate",
+    "rarity": "Classified",
+    "float_range": [0.06, 0.80],
+    "type": "gun",
+    "id": 8,
+}
+
+skin9 = {
+    "weapon": "P90",
+    "skin": "Death by Kitty",
+    "rarity": "Covert",
+    "float_range": [0.08, 0.32],
+    "type": "gun",
+    "id": 9,
+}
+
+knife1 = {
+    "weapon": "Bayonet",
+    "skin": [
+        "",
+        "Blue Steel",
+        "Boreal Forest",
+        "Case Hardened",
+        "Crimson Web",
+        "Fade",
+        "Forest DDPAT",
+        "Night",
+        "Safari Mesh",
+        "Scorched",
+        "Slaughter",
+        "Stained",
+        "Urban Masked",
+    ],
+    "rarity": "Exceedingly Rare",
+    "float_range": [0.00, 1.00],
+    "type": "knife",
+    "id": "k1",
+}
+
+knife2 = {
+    "weapon": "Flip Knife",
+    "skin": [
+        "",
+        "Blue Steel",
+        "Boreal Forest",
+        "Case Hardened",
+        "Crimson Web",
+        "Fade",
+        "Forest DDPAT",
+        "Night",
+        "Safari Mesh",
+        "Scorched",
+        "Slaughter",
+        "Stained",
+        "Urban Masked",
+    ],
+    "rarity": "Exceedingly Rare",
+    "float_range": [0.00, 1.00],
+    "type": "knife",
+    "id": "k2",
+}
+
+knife3 = {
+    "weapon": "Gut Knife",
+    "skin": [
+        "",
+        "Blue Steel",
+        "Boreal Forest",
+        "Case Hardened",
+        "Crimson Web",
+        "Fade",
+        "Forest DDPAT",
+        "Night",
+        "Safari Mesh",
+        "Scorched",
+        "Slaughter",
+        "Stained",
+        "Urban Masked",
+    ],
+    "rarity": "Exceedingly Rare",
+    "float_range": [0.00, 1.00],
+    "type": "knife",
+    "id": "k3",
+}
+
+knife4 = {
+    "weapon": "Karambit",
+    "skin": [
+        "",
+        "Blue Steel",
+        "Boreal Forest",
+        "Case Hardened",
+        "Crimson Web",
+        "Fade",
+        "Forest DDPAT",
+        "Night",
+        "Safari Mesh",
+        "Scorched",
+        "Slaughter",
+        "Stained",
+        "Urban Masked",
+    ],
+    "rarity": "Exceedingly Rare",
+    "float_range": [0.00, 1.00],
+    "type": "knife",
+    "id": "k4",
+}
+
+knife5 = {
+    "weapon": "M9 Bayonet",
+    "skin": [
+        "",
+        "Blue Steel",
+        "Boreal Forest",
+        "Case Hardened",
+        "Crimson Web",
+        "Fade",
+        "Forest DDPAT",
+        "Night",
+        "Safari Mesh",
+        "Scorched",
+        "Slaughter",
+        "Stained",
+        "Urban Masked",
+    ],
+    "rarity": "Exceedingly Rare",
+    "float_range": [0.00, 1.00],
+    "type": "knife",
+    "id": "k5",
+}
+
+mil_spec = [skin1, skin2, skin3]
+restricted = [skin4, skin5, skin6]
+classified = [skin7, skin8]
+covert = [skin9]
+exceedingly_rare = [knife1, knife2, knife3, knife4, knife5]
