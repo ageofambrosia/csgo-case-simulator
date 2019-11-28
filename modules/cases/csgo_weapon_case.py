c9fbfa1bd918f11e61fa5ac061eb96814aaa5a16
Vidhan <vidh.bhatt@gmail.com>
2019-11-28 14:48:33 -0500

    move modules to a seperate folder

---
+++ modules/cases/csgo_weapon_case.py
@@ -0,0 +1,204 @@
+case_full_name = "CS:GO Weapon Case"
+case_name = "csgo_weapon_case"
+
+skin1 = {
+    "weapon": "MP7",
+    "skin": "Skulls",
+    "rarity": "Mil-Spec",
+    "float_range": [0.10, 0.26],
+    "type": "gun",
+    "id": 1,
+}
+
+skin2 = {
+    "weapon": "AUG",
+    "skin": "Wings",
+    "rarity": "Mil-Spec",
+    "float_range": [0.00, 0.14],
+    "type": "gun",
+    "id": 2,
+}
+
+skin3 = {
+    "weapon": "SG 553",
+    "skin": "Ultraviolet",
+    "rarity": "Mil-Spec",
+    "float_range": [0.06, 0.80],
+    "type": "gun",
+    "id": 3,
+}
+
+skin4 = {
+    "weapon": "USP-S",
+    "skin": "Dark Water",
+    "rarity": "Restricted",
+    "float_range": [0.10, 0.26],
+    "type": "gun",
+    "id": 4,
+}
+
+skin5 = {
+    "weapon": "M4A1-S",
+    "skin": "Dark Water",
+    "rarity": "Restricted",
+    "float_range": [0.10, 0.26],
+    "type": "gun",
+    "id": 5,
+}
+
+skin6 = {
+    "weapon": "Glock-18",
+    "skin": "Dragon Tattoo",
+    "rarity": "Restricted",
+    "float_range": [0.00, 0.08],
+    "type": "gun",
+    "id": 6,
+}
+
+skin7 = {
+    "weapon": "Desert Eagle",
+    "skin": "Hypnotic",
+    "rarity": "Classified",
+    "float_range": [0.00, 0.08],
+    "type": "gun",
+    "id": 7,
+}
+
+skin8 = {
+    "weapon": "AK-47",
+    "skin": "Case Hardened",
+    "rarity": "Classified",
+    "float_range": [0.00, 1.00],
+    "type": "gun",
+    "id": 8,
+}
+
+skin9 = {
+    "weapon": "AWP",
+    "skin": "Lightning Strike",
+    "rarity": "Covert",
+    "float_range": [0.00, 0.08],
+    "type": "gun",
+    "id": 9,
+}
+
+knife1 = {
+    "weapon": "Bayonet",
+    "skin": [
+        "",
+        "Blue Steel",
+        "Boreal Forest",
+        "Case Hardened",
+        "Crimson Web",
+        "Fade",
+        "Forest DDPAT",
+        "Night",
+        "Safari Mesh",
+        "Scorched",
+        "Slaughter",
+        "Stained",
+        "Urban Masked",
+    ],
+    "rarity": "Exceedingly Rare",
+    "float_range": [0.00, 1.00],
+    "type": "knife",
+    "id": "k1",
+}
+
+knife2 = {
+    "weapon": "Flip Knife",
+    "skin": [
+        "",
+        "Blue Steel",
+        "Boreal Forest",
+        "Case Hardened",
+        "Crimson Web",
+        "Fade",
+        "Forest DDPAT",
+        "Night",
+        "Safari Mesh",
+        "Scorched",
+        "Slaughter",
+        "Stained",
+        "Urban Masked",
+    ],
+    "rarity": "Exceedingly Rare",
+    "float_range": [0.00, 1.00],
+    "type": "knife",
+    "id": "k2",
+}
+
+knife3 = {
+    "weapon": "Gut Knife",
+    "skin": [
+        "",
+        "Blue Steel",
+        "Boreal Forest",
+        "Case Hardened",
+        "Crimson Web",
+        "Fade",
+        "Forest DDPAT",
+        "Night",
+        "Safari Mesh",
+        "Scorched",
+        "Slaughter",
+        "Stained",
+        "Urban Masked",
+    ],
+    "rarity": "Exceedingly Rare",
+    "float_range": [0.00, 1.00],
+    "type": "knife",
+    "id": "k3",
+}
+
+knife4 = {
+    "weapon": "Karambit",
+    "skin": [
+        "",
+        "Blue Steel",
+        "Boreal Forest",
+        "Case Hardened",
+        "Crimson Web",
+        "Fade",
+        "Forest DDPAT",
+        "Night",
+        "Safari Mesh",
+        "Scorched",
+        "Slaughter",
+        "Stained",
+        "Urban Masked",
+    ],
+    "rarity": "Exceedingly Rare",
+    "float_range": [0.00, 1.00],
+    "type": "knife",
+    "id": "k4",
+}
+
+knife5 = {
+    "weapon": "M9 Bayonet",
+    "skin": [
+        "",
+        "Blue Steel",
+        "Boreal Forest",
+        "Case Hardened",
+        "Crimson Web",
+        "Fade",
+        "Forest DDPAT",
+        "Night",
+        "Safari Mesh",
+        "Scorched",
+        "Slaughter",
+        "Stained",
+        "Urban Masked",
+    ],
+    "rarity": "Exceedingly Rare",
+    "float_range": [0.00, 1.00],
+    "type": "knife",
+    "id": "k5",
+}
+
+mil_spec = [skin1, skin2, skin3]
+restricted = [skin4, skin5, skin6]
+classified = [skin7, skin8]
+covert = [skin9]
+exceedingly_rare = [knife1, knife2, knife3, knife4, knife5]
---
+++ modules/odds.py
@@ -0,0 +1,19 @@
+# """
+
+mil_spec = 79.92
+restricted = mil_spec + 15.98
+classified = restricted + 3.2
+covert = classified + 0.64
+exceedingly_rare = covert + 0.26
+stattrak = 10
+
+"""
+
+mil_spec = 0
+restricted = mil_spec + 0
+classified = restricted + 0
+covert = classified + 0
+exceedingly_rare = covert + 100
+stattrak = 10
+
+"""
